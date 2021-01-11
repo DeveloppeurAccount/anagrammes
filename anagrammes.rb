c63ac81fb880fcf1983b3b8acebe9c12aaabadfe
Developpeur Account <myadressemailisthat@gmail.com>
2021-01-11 09:51:50 +0100

    add file

---
+++ anagrammes.rb
@@ -0,0 +1,19 @@
+anagram_file = ARGV[1]
+
+word_list = []
+position = 0
+File.foreach(anagram_file) { |line| word_list[position] = line.chomp.split(''); position += 1 }
+
+anagrams = []
+line = 0
+while (line < word_list.length)
+    difference = word_to_compare - word_list[line]
+    if difference.empty? == true
+        anagrams << word_list[line].join('')
+    end
+    line += 1
+end
+
+puts anagrams.inspect
+
+
